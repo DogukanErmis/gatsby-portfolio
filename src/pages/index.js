257435407f40324d30e09f53f02278d11c120498
D-Ermis <ermisdogukan@gmail.com>
2021-08-02 22:01:30 +0200

    change helmet title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,12 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Dogukan Ermis - Web Application Developer'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta
+          name="description"
+          content={description || 'Dogukan Ermis - Web Application Developer'}
+        />
       </Helmet>
       <App />
     </>
