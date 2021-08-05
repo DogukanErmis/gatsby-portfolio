65068f388c7a688d83823d56f0d6f9babd39a3d2
dogukanermis <ermisd+github@pm.me>
2021-08-05 16:50:46 +0200

    update about me description

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -38,15 +38,17 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  I&apos;m a Belgium based web application developer focused on crafting clean and
+                  user-friendly interfaces. I have done work in software development,
+                  front-end/back-end web and video game development.
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  I enjoy being challenged and engaging with projects that require me to work
+                  outside my comfort and knowledge set, as continuing to learn new languages and
+                  development techniques are important to me and the success of any organization.
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  <b>Skills</b>: JavaScript, TypeScript, ReactJS, Redux, NodeJS, SQL, HTML/CSS
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
