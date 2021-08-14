37d2315e45de7caabde2059d3f245a86cdb04393
dogukanermis <ermisd+github@pm.me>
2021-08-14 19:34:48 +0200

    remove fade effect from title

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,20 +20,19 @@ const Header = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <Fade
+        {/* <Fade
           left={isDesktop}
           bottom={isMobile}
           duration={1000}
           delay={500}
           distance="30px"
-          ssrFadeout
-        >
-          <h1 className="hero-title">
-            <span className="text-color-main">Dogukan Ermis</span>
-            <br />
-            Software Developer
-          </h1>
-        </Fade>
+        > */}
+        <h1 className="hero-title">
+          <span className="text-color-main">Dogukan Ermis</span>
+          <br />
+          Software Developer
+        </h1>
+        {/* </Fade> */}
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <Link to="about" className="cta-btn cta-btn--hero">
