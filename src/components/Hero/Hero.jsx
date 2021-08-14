223f25fd7e2966522e1a3b09c7b5a2d3ef27db58
dogukanermis <ermisd+github@pm.me>
2021-08-14 20:00:17 +0200

    add fade effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,11 +20,13 @@ const Header = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <h1 className="hero-title">
-          <span className="text-color-main">Dogukan Ermis</span>
-          <br />
-          Software Developer
-        </h1>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <h1 className="hero-title">
+            <span className="text-color-main">Dogukan Ermis</span>
+            <br />
+            Software Developer
+          </h1>
+        </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <Link to="about" className="cta-btn cta-btn--hero">
