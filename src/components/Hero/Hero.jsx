3ab4df963fb1b4d45e41d1371b577ebf71814b56
dogukanermis <ermisd+github@pm.me>
2021-08-14 20:12:04 +0200

    replace fade with swing effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
+// import Fade from 'react-reveal/Fade';
+import Swing from 'react-reveal/Swing';
 import { Link } from 'react-scroll';
 
-const Header = () => {
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-    }
-  }, []);
-
-  return (
-    <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            <span className="text-color-main">Dogukan Ermis</span>
-            <br />
-            Software Developer
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <Link to="about" className="cta-btn cta-btn--hero">
-              Know more
-            </Link>
-          </p>
-        </Fade>
-      </Container>
-    </section>
-  );
-};
+const Header = () => (
+  <section id="hero" className="jumbotron">
+    <Container>
+      {/* <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px"> */}
+      <h1 className="hero-title">
+        <span className="text-color-main">Dogukan Ermis</span>
+        <br />
+        Software Developer
+      </h1>
+      {/* </Fade> */}
+      {/* <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px"> */}
+      <p className="hero-cta">
+        <Swing bottom duration={1000} delay={800} distance="30px">
+          <Link to="about" className="cta-btn cta-btn--hero">
+            Know more
+          </Link>
+        </Swing>
+      </p>
+      {/* </Fade> */}
+    </Container>
+  </section>
+);
 
 export default Header;
