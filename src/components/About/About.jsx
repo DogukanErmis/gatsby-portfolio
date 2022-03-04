21fe423b672cc557a982fc8c14621bc2c2acce75
dogukanermis <ermisd+github@pm.me>
2022-03-04 11:18:59 +0100

    update skills

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,10 +42,11 @@ const About = () => {
                   popular websites.
                 </p>
                 <p className="about-wrapper__info-text">
-                  <u>Skills:</u> JavaScript, ReactJS, Redux, Node.js, SQL, MongoDB, PHP, HTML/CSS
+                  <u>Skills:</u> JavaScript, TypeScript, React, Node.js, Java (Spring, Hibernate),
+                  SQL, MongoDB, PHP
                 </p>
                 <p className="about-wrapper__info-text">
-                  <u>Learning:</u> TypeScript, Go
+                  <u>Methods:</u> Agile, Clean Code, TDD, FP
                 </p>
                 <span className="d-flex mt-3">
                   <a
